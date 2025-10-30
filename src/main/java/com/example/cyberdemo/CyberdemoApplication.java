8ea6af803d527faad6005ec4ea3d1a8a835f83cc
Arnaud Manuel <arnaudmanuel635@gmail.com>
2025-10-30 13:13:49 +0100

    Initial commit with tests and CI

---
+++ src/main/java/com/example/cyberdemo/CyberdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyberdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cyberdemo/CyberdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyberdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
